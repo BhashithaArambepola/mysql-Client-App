247eb4d00f16201569b9495c6c6c41380ccf6e08
Bhashitha Arambepola <42783927+BhashithaArambepola@users.noreply.github.com>
2022-02-25 13:05:05 +0530

    Validate user inputs in the login form

--- mysql-client/src/controller/mainFormController.java
+++ mysql-client/src/controller/mainFormController.java
@@ -2,8 +2,11 @@ package controller;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class mainFormController {
     public TextField txtHost;
@@ -18,15 +21,39 @@ public class mainFormController {
     }
 
     public void btnConnectOnAction(ActionEvent actionEvent) {
-        
+        if(txtHost.getText().trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR,"Host can't be empty").show();
+            txtHost.requestFocus();
+            txtHost.selectAll();
+            return;
+        }
+        else if(!txtPort.getText().matches("\\d+")){
+            new Alert(Alert.AlertType.ERROR,"Error! invalid Port").show();
+            txtPort.requestFocus();
+            txtPort.selectAll();
+            return;
+        }
+        else if(txtUserName.getText().trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR,"UserName can't be empty").show();
+            txtUserName.requestFocus();
+            txtUserName.selectAll();
+            return;
+        }
+
         String command= String.format("mysql -h %s -u %s -p%s --port %s",
                 txtHost.getText(),
                 txtUserName.getText(),
                 txtPassword.getText(),
                 txtPort.getText());
+        try {
+            Process mysql = Runtime.getRuntime().exec(command);
+            System.out.println(mysql.waitFor());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void btnExitOnAction(ActionEvent actionEvent) {
-        System.exit();
+        System.exit(0);
     }
 }
